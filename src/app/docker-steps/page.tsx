fa1b007b3242f90ccb2db21367e47a5e0edbc0d7
mahesh <mahesh123@yopmail.com>
2026-07-31 20:51:08 +0530

    proper cicd pipe line creation

---
+++ src/app/docker-steps/page.tsx
@@ -0,0 +1,10 @@
+const DockerSteps = () => {
+  return (
+    <div>
+      <h1>Docker Steps</h1>
+      <p>This is a simple page to demonstrate Docker steps.</p>
+    </div>
+  );
+};
+
+export default DockerSteps;
